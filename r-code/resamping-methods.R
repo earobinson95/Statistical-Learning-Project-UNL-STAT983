fe59a28539210d44e5209d65372d7f1fdcea96dc
alisonkleffner <akleffner@huskers.unl.edu>
2021-03-29 21:06:47 -0500

    Got a SMOTE Resampling method running. Two options, should discuss which we we want to use.

--- r-code/resamping-methods.R
+++ r-code/resamping-methods.R
@@ -33,19 +33,37 @@ wine2 <- sort(sample(nrow(wine), nrow(wine)*0.5))
 train_wine <- wine[wine2,]
 table(train_wine $ binary_class) #0.06773399 in rare class (not bad)
 
-
+#Attempt 1: library(peformanceEstimation)
 names(wine)
 form <- formula(binary_class ~ .)
-SMOTEData <- smote(form,train_wine,perc.under=20,k=5,perc.over = 20)
+SMOTEData <- smote(form,train_wine,k=4,perc.under = 100, perc.over = 100)
 
 
 table(SMOTEData$binary_class) 
+
 #currently it's not adding any to minority set, but its adding a ton to majority set
 
+#Attempt 2 library(imbalance) - This just generate the samples we would add to data set 
+#so numInstances = 100, samples minority class 100 times. Just add this to dataset?
 
+SMOTEData <- mwmote(
+  train_wine,
+  numInstances = 100,
+  kNoisy = 5,
+  threshold = 5,
+  cmax = 2,
+  cclustering = 3,
+  classAttr = "binary_class"
+)
+table(SMOTEData$binary_class) 
 
+#Attempt 3 library(smotefamily) - in my opion this is the winner
+#https://www.rdocumentation.org/packages/smotefamily/versions/1.3.1/topics/SMOTE
 
+SMOTEData <- SMOTE(train_wine[,c(1:11)], train_wine[,12], K = 5, dup_size = 0)
 
+data <- SMOTEData$data
 
+table(data$class) 
 
 ### Undersampling Method ###
